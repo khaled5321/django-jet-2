19a78286c5ae5ecc97f1a4253fdf76f72693fad3
Khaled Emad <khaledemad656@gmail.com>
2024-06-01 17:45:27 +0300

    Temp Fix django.jQuery is not a function thanks to:https://github.com/django-import-export/django-import-export/issues/618

--- jet/dashboard/urls.py
+++ jet/dashboard/urls.py
@@ -1,4 +1,4 @@
-from django.urls import re_path
+from django.urls import path
 from django.views.i18n import JavaScriptCatalog
 
 from jet.dashboard import dashboard
@@ -13,50 +13,21 @@ from jet.dashboard.views import (
 )
 
 javascript_catalog = JavaScriptCatalog.as_view()
-app_name = 'dashboard'
+app_name = "dashboard"
 
 urlpatterns = [
-    re_path(
-        r'^module/(?P<pk>\d+)/$',
-        UpdateDashboardModuleView.as_view(),
-        name='update_module'
-    ),
-    re_path(
-        r'^update_dashboard_modules/$',
-        update_dashboard_modules_view,
-        name='update_dashboard_modules'
-    ),
-    re_path(
-        r'^add_user_dashboard_module/$',
-        add_user_dashboard_module_view,
-        name='add_user_dashboard_module'
-    ),
-    re_path(
-        r'^update_dashboard_module_collapse/$',
+    path("module/<int:pk>/", UpdateDashboardModuleView.as_view(), name="update_module"),
+    path("update_dashboard_modules/", update_dashboard_modules_view, name="update_dashboard_modules"),
+    path("add_user_dashboard_module/", add_user_dashboard_module_view, name="add_user_dashboard_module"),
+    path(
+        "update_dashboard_module_collapse/",
         update_dashboard_module_collapse_view,
-        name='update_dashboard_module_collapse'
+        name="update_dashboard_module_collapse",
     ),
-    re_path(
-        r'^remove_dashboard_module/$',
-        remove_dashboard_module_view,
-        name='remove_dashboard_module'
-    ),
-    re_path(
-        r'^load_dashboard_module/(?P<pk>\d+)/$',
-        load_dashboard_module_view,
-        name='load_dashboard_module'
-    ),
-    re_path(
-        r'^reset_dashboard/$',
-        reset_dashboard_view,
-        name='reset_dashboard'
-    ),
-    re_path(
-        r'^jsi18n/$',
-        javascript_catalog,
-        {'packages': 'jet'},
-        name='jsi18n'
-    ),
+    path("remove_dashboard_module/", remove_dashboard_module_view, name="remove_dashboard_module"),
+    path("load_dashboard_module/<int:pk>/", load_dashboard_module_view, name="load_dashboard_module"),
+    path("reset_dashboard/", reset_dashboard_view, name="reset_dashboard"),
+    path("jsi18n/", javascript_catalog, {"packages": "jet"}, name="jsi18n"),
 ]
 
 urlpatterns += dashboard.urls.get_urls()
--- jet/dashboard/views.py
+++ jet/dashboard/views.py
@@ -20,9 +20,9 @@ from jet.utils import SuccessMessageMixin, get_app_list, user_is_authenticated
 
 class UpdateDashboardModuleView(SuccessMessageMixin, UpdateView):
     model = UserDashboardModule
-    fields = ('title',)
-    template_name = 'jet.dashboard/update_module.html'
-    success_message = _('Widget was successfully updated')
+    fields = ("title",)
+    template_name = "jet.dashboard/update_module.html"
+    success_message = _("Widget was successfully updated")
     object = None
     module = None
 
@@ -31,80 +31,83 @@ class UpdateDashboardModuleView(SuccessMessageMixin, UpdateView):
 
     def get_success_url(self):
         if self.object.app_label:
-            return reverse('admin:app_list', kwargs={'app_label': self.object.app_label})
+            return reverse("admin:app_list", kwargs={"app_label": self.object.app_label})
         else:
-            return reverse('admin:index')
+            return reverse("admin:index")
 
     def get_module(self):
-        object = self.object if getattr(self, 'object', None) is not None else self.get_object()
+        object = self.object if getattr(self, "object", None) is not None else self.get_object()
         return object.load_module()
 
     def get_settings_form_kwargs(self):
-        kwargs = {
-            'initial': self.module.settings
-        }
+        kwargs = {"initial": self.module.settings}
 
-        if self.request.method in ('POST', 'PUT'):
-            kwargs.update({
-                'data': self.request.POST,
-                'files': self.request.FILES,
-            })
+        if self.request.method in ("POST", "PUT"):
+            kwargs.update(
+                {
+                    "data": self.request.POST,
+                    "files": self.request.FILES,
+                }
+            )
         return kwargs
 
     def get_settings_form(self):
         if self.module.settings_form:
             form = self.module.settings_form(**self.get_settings_form_kwargs())
-            if hasattr(form, 'set_module'):
+            if hasattr(form, "set_module"):
                 form.set_module(self.module)
             return form
 
     def get_children_formset_kwargs(self):
         kwargs = {
-            'initial': self.module.children,
-            'prefix': 'children',
+            "initial": self.module.children,
+            "prefix": "children",
         }
 
-        if self.request.method in ('POST', 'PUT'):
-            kwargs.update({
-                'data': self.request.POST,
-                'files': self.request.FILES,
-            })
+        if self.request.method in ("POST", "PUT"):
+            kwargs.update(
+                {
+                    "data": self.request.POST,
+                    "files": self.request.FILES,
+                }
+            )
         return kwargs
 
     def get_children_formset(self):
         if self.module.child_form:
-            return formset_factory(self.module.child_form, can_delete=True, extra=1)(**self.get_children_formset_kwargs())
+            return formset_factory(self.module.child_form, can_delete=True, extra=1)(
+                **self.get_children_formset_kwargs()
+            )
 
     def clean_children_data(self, children):
-        children = list(filter(
-            lambda item: isinstance(item, dict) and item and item.get('DELETE') is not True,
-            children
-        ))
+        children = list(
+            filter(lambda item: isinstance(item, dict) and item and item.get("DELETE") is not True, children)
+        )
         for item in children:
-            item.pop('DELETE')
+            item.pop("DELETE")
         return children
 
     def get_current_app(self):
-        app_list = get_app_list({'request': self.request})
+        app_list = get_app_list({"request": self.request})
 
         for app in app_list:
-            if app.get('app_label', app.get('name')) == self.object.app_label:
+            if app.get("app_label", app.get("name")) == self.object.app_label:
                 return app
 
     def get_context_data(self, **kwargs):
         data = super(UpdateDashboardModuleView, self).get_context_data(**kwargs)
-        data['title'] = _('Change')
-        data['module'] = self.module
-        data['settings_form'] = self.get_settings_form()
-        data['children_formset'] = self.get_children_formset()
-        data['child_name'] = self.module.child_name if self.module.child_name else _('Items')
-        data['child_name_plural'] = self.module.child_name_plural if self.module.child_name_plural else _('Items')
-        data['app'] = self.get_current_app()
+        data["title"] = _("Change")
+        data["module"] = self.module
+        data["settings_form"] = self.get_settings_form()
+        data["children_formset"] = self.get_children_formset()
+        data["child_name"] = self.module.child_name if self.module.child_name else _("Items")
+        data["child_name_plural"] = self.module.child_name_plural if self.module.child_name_plural else _("Items")
+        data["app"] = self.get_current_app()
         return data
 
     def dispatch(self, request, *args, **kwargs):
         if not self.has_permission(request):
-            index_path = reverse('admin:index')
+            index_path = reverse("admin:index")
             return HttpResponseRedirect(index_path)
 
         self.object = self.get_object()
@@ -120,14 +123,14 @@ class UpdateDashboardModuleView(SuccessMessageMixin, UpdateView):
         if settings_form:
             if settings_form.is_valid():
                 settings = settings_form.cleaned_data
-                data['settings'] = self.module.dump_settings(settings)
+                data["settings"] = self.module.dump_settings(settings)
             else:
                 return self.form_invalid(self.get_form(self.get_form_class()))
 
         if children_formset:
             if children_formset.is_valid():
                 self.module.children = self.clean_children_data(children_formset.cleaned_data)
-                data['children'] = self.module.dump_children()
+                data["children"] = self.module.dump_children()
             else:
                 return self.form_invalid(self.get_form(self.get_form_class()))
 
@@ -136,109 +139,109 @@ class UpdateDashboardModuleView(SuccessMessageMixin, UpdateView):
         return super(UpdateDashboardModuleView, self).post(request, *args, **kwargs)
 
     def form_valid(self, form):
-        if 'settings' in form.data:
-            form.instance.settings = form.data['settings']
-        if 'children' in form.data:
-            form.instance.children = form.data['children']
+        if "settings" in form.data:
+            form.instance.settings = form.data["settings"]
+        if "children" in form.data:
+            form.instance.children = form.data["children"]
         return super(UpdateDashboardModuleView, self).form_valid(form)
 
 
 @require_POST
 def update_dashboard_modules_view(request):
-    result = {'error': False}
+    result = {"error": False}
     form = UpdateDashboardModulesForm(request, request.POST)
 
     if form.is_valid():
         form.save()
     else:
-        result['error'] = True
+        result["error"] = True
 
     return JsonResponse(result)
 
 
 @require_POST
 def add_user_dashboard_module_view(request):
-    result = {'error': False}
+    result = {"error": False}
     form = AddUserDashboardModuleForm(request, request.POST)
 
     if form.is_valid():
         module = form.save()
-        result['id'] = module.pk
-        messages.success(request, _('Widget has been successfully added'))
+        result["id"] = module.pk
+        messages.success(request, _("Widget has been successfully added"))
 
         if module.app_label:
-            result['success_url'] = reverse('admin:app_list', kwargs={'app_label': module.app_label})
+            result["success_url"] = reverse("admin:app_list", kwargs={"app_label": module.app_label})
         else:
-            result['success_url'] = reverse('admin:index')
+            result["success_url"] = reverse("admin:index")
     else:
-        result['error'] = True
+        result["error"] = True
 
     return JsonResponse(result)
 
 
 @require_POST
 def update_dashboard_module_collapse_view(request):
-    result = {'error': False}
+    result = {"error": False}
 
     try:
-        instance = UserDashboardModule.objects.get(pk=request.POST.get('id'))
+        instance = UserDashboardModule.objects.get(pk=request.POST.get("id"))
         form = UpdateDashboardModuleCollapseForm(request, request.POST, instance=instance)
 
         if form.is_valid():
             module = form.save()
-            result['collapsed'] = module.collapsed
+            result["collapsed"] = module.collapsed
         else:
-            result['error'] = True
+            result["error"] = True
     except UserDashboardModule.DoesNotExist:
-        result['error'] = True
+        result["error"] = True
 
     return JsonResponse(result)
 
 
 @require_POST
 def remove_dashboard_module_view(request):
-    result = {'error': False}
+    result = {"error": False}
 
     try:
-        instance = UserDashboardModule.objects.get(pk=request.POST.get('id'))
+        instance = UserDashboardModule.objects.get(pk=request.POST.get("id"))
         form = RemoveDashboardModuleForm(request, request.POST, instance=instance)
 
         if form.is_valid():
             form.save()
         else:
-            result['error'] = True
+            result["error"] = True
     except UserDashboardModule.DoesNotExist:
-        result['error'] = True
+        result["error"] = True
 
     return JsonResponse(result)
 
 
 @require_GET
 def load_dashboard_module_view(request, pk):
-    result = {'error': False}
+    result = {"error": False}
 
     try:
         if not user_is_authenticated(request.user) or not request.user.is_staff:
-            raise ValidationError('error')
+            raise ValidationError("error")
 
         instance = UserDashboardModule.objects.get(pk=pk, user=request.user.pk)
         module_cls = instance.load_module()
-        module = module_cls(model=instance, context={'request': request})
-        result['html'] = module.render()
+        module = module_cls(model=instance, context={"request": request})
+        result["html"] = module.render()
     except (ValidationError, UserDashboardModule.DoesNotExist):
-        result['error'] = True
+        result["error"] = True
 
     return JsonResponse(result)
 
 
 @require_POST
 def reset_dashboard_view(request):
-    result = {'error': False}
+    result = {"error": False}
     form = ResetDashboardForm(request, request.POST)
 
     if form.is_valid():
         form.save()
     else:
-        result['error'] = True
+        result["error"] = True
 
     return JsonResponse(result)
