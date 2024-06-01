2fc2242ce69c78ce53d7abbebc208ffb00af2f91
Khaled Emad <khaledemad656@gmail.com>
2024-06-01 17:52:52 +0300

    Enable Themes by default in testing app

--- jet/tests/settings.py
+++ jet/tests/settings.py
@@ -75,3 +75,16 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 JET_INDEX_DASHBOARD = "jet.tests.dashboard.TestIndexDashboard"
 JET_APP_INDEX_DASHBOARD = "jet.tests.dashboard.TestAppIndexDashboard"
+
+JET_THEMES = [
+    {
+        "theme": "default",  # theme folder name
+        "color": "#47bac1",  # color of the theme's button in user menu
+        "title": "Default",  # theme title
+    },
+    {"theme": "green", "color": "#44b78b", "title": "Green"},
+    {"theme": "light-green", "color": "#2faa60", "title": "Light Green"},
+    {"theme": "light-violet", "color": "#a464c4", "title": "Light Violet"},
+    {"theme": "light-blue", "color": "#5EADDE", "title": "Light Blue"},
+    {"theme": "light-gray", "color": "#222", "title": "Light Gray"},
+]
